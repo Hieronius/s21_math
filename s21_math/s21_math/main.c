0770c9b97090dfac963953aa4f7dfee27878f360
Arsenty Halimovsky <ghost12@yandex.ru>
2024-01-30 07:29:58 +0300

    Start New project

---
+++ s21_math/s21_math/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  s21_math
+//
+//  Created by Арсентий Халимовский on 30.01.2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
