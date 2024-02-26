158a0e75746d84ad92630e002fe5535b42aa303a
Arsenty Halimovsky <ghost12@yandex.ru>
2024-02-26 05:32:26 +0300

    Add check.h description
    How to build a single test, test case, test set and how to run it

--- s21_math/s21_math/main.c
+++ s21_math/s21_math/main.c
@@ -1,14 +1,254 @@
-//
-//  main.c
-//  s21_math
-//
-//  Created by Арсентий Халимовский on 30.01.2024.
-//
-
 #include <stdio.h>
+#include <limits.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+// MARK: Арсентий.
+/*
+ 1. Какие глобальные переменные нужны ✅
+ 2. Проверки от дураков:
+   В решениях у ребят не увидел проверок в функциях вообще.
+ 3. Разобрать тесты для функций math.h:
+ 
+   * Алгоритм создания тестов для одной функции:
+ 
+        - создание функции START_TEST(название функции) с помощью assert
+        - создание набора тестов Suite
+        - добавление теста с assert в виде case в набор тестов Suite
+        - запуск набора тестов Suite в main. Для каждой функции свой файл и main
+ 
+    * Из чего состоит стандартный тест:
+ 
+        Пример: ck_assert_int_eq(s21_abs(0), 0)
+ 
+        - START_TEST - макрос, который означает начало нового test case
+ 
+        - ck_assert_int_eq - макрос, который проверяет корретный output функции
+            ck - аббревиатура для check.h
+            assert - означает, что функция должна вернуть true, иначе fail
+            int - означает, что функция должна иметь тип данных "int"
+            eq - означает, что входное значение функции и результат должны быть равны
+            
+ 
+            Первый аргумент тест-функции принимает нашу функцию с входным аргументом
+            Второй аргумент тес-функции принимает желаемый результат
+ 
+                Первый аргумент - s21_abs(0). Подаем 0 в функцию
+                Второй аргумент - 0. Хотим получить 0 как результат
+ 
+        - END_TEST - макрос, который означает конец test case
+ 
+      * Из чего состоит стандартный набор тестов:
+ 
+        Пример: Suite *s21_math_abs_suite() {
+                Suite *suite;
+                TCase *tc_abs;
+
+                suite = suite_create("s21_abs");
+
+                tc_abs_usual = tcase_create("s21_abs");
+                tcase_add_test(tc_abs);
+
+                suite_add_tcase(suite, tc_abs);
+
+                return suite;
+                }
+
+        - Suite *названиеФункции_suite() - возвращает указатель на struct Suite
+        - Suite *suite - указатель на Suite, чтобы хранить тестовые данные
+        - TCase *названиеФункции - указатель на test case тестируемой функции (пустой)
+        - suite = suite_create(название функции) - создаем новый набор тестов для функции
+        - TCase *названиеФункции = tcase_create("названиеФункции") - присваиваем имя для test case
+        - tcase_add_test(названиеФункции) - добавляем нужные тесты в test case
+        - suite_add_tcase(s, Tcase) - добавляем test case в набор тестов
+        - return suite - возвращаем заполненный набор тестов
+ 
+      * Запуск тестов:
+ 
+        Пример: int main() {
+                Suite *suite;
+                int failed = 0;
+                SRunner *runner;
+
+                suite = s21_math_abs_suite();
+                runner = srunner_create(suite);
+
+                srunner_run_all(runner, CK_NORMAL);
+                failed = srunner_ntests_failed(runner);
+                srunner_free(runner);
+                return (failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+                }
+        
+        - int main {} - место для запуска тестов
+        - Suite *suite - указатель на пустой набор тестов
+        - int failed = 0 - место для хранения счетчика проваленных тестов
+        - SRunner *runner - переменная отвечает за запуск тестов
+        - suite = s21_math_abs_suite() - присваеваем конкретный набор тестов
+        - runner = srunner_create(suite) - "создать запуск тестов (набор тестов)"
+        - srunner_run_all(runner, CK_NORMAL) - стандартный запуск тестов
+            - CK_NORMAL - означает уровень деталей выводимых на консоль. Без ньюансов непонятных обычному пользователю
+        - failed = srunner_ntests_failed(runner) - запись числа проваленных тестов
+        - srunner_free(runner) - освобождение памяти, выделенной на тесты
+        - return (failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE - если проваленных тестов нет, верни TRUE. В противном случае верни False
+ */
+
+// MARK: Влад. Разобрать какие глобальные переменные и проверки от дураков, разобрать тесты для функций math.h
+
+// MARK: - Константы для math.h
+
+#define S21_PI 3.1415926535897932 // (возможно надо поставить L - в конец числа, значит long. Тут не 16 знаков, но число округлено верно. Возможно нам больше не надо.
+#define S21_EXP // Нашел такое число - 2.7182818284590452. Не знаю с чем связано
+#define S21_EPS 1e-9 // Находил разные вариации: 1e-10, 1e-17. Полагаю такая высокая точность нам не нужна
+#define S21_INF 1.0 / 0.0 // есть еще и негативная бесконечность: -1.0 / 0.0
+#define S21_NAN 0.0 / 0.0 // полагаю, потому что - "0 на 0 делить нельзя", вот и выходит "не число".
+
+
+// MARK: - Уже готовы (добавить проверки)
+int s21_abs(int x); // вычисляет абсолютное значение целого числа + // 0 ✅
+long double s21_fabs(double x); // вычисляет абсолютное значение числа с плавающей ✅ точкой +
+
+long double s21_ceil(double x); // возвращает ближайшее целое число, не меньшее заданного значения +
+long double s21_floor(double x); // возвращает ближайшее целое число, не превышающее заданное значение + ✅
+long double s21_fmod(double x, double y); // остаток операции деления с плавающей ✅ точкой +
+long double s21_exp(double x); // вычисляет экспоненту числа с плавающей точкой с помощью ряда Тейлора ✅
+long double s21_sqrt(double x); // вычисляет квадратный корень ✅
+
+// Простые функции
+
+// MARK: - Саша
+long double s21_cos(double x); // вычисляет косинус ❗️
+long double s21_sin(double x); //  вычисляет синус ❗️
+long double s21_tan(double x); // вычисляет тангенс ❗️
+
+// Будут использованы стандартные sqrt и pow для реализации функций, затем заменим на созданные прототипы
+
+long double s21_acos(double x); //вычисляет арккосинус // требует Pow, sqrt и cos ❗️
+long double s21_asin(double x); //вычисляет арксинус // требует Pow, sqrt и sin ❗️
+long double s21_atan(double x); // вычисляет арктангенс ❗️
+
+// MARK: - Руслан
+long double s21_pow(double base, double exp); // возводит число в заданную степень ❗️ // 1
+// factorial (рекурсия)
+// Если будет время, пожалуйста, напиши простенький makefile без проверок на macOS/linux.
+
+// MARK: - Никита
+long double s21_log(double x); // вычисляет натуральный логарифм ❗️
+
+
+// MARK: Info and notes
+
+/*
+ 
+ https://github.com/SolutionOP/Math - S21_Math from one of the students with makefile, libraries and tests
+ 
+ https://github.com/MustafaNatur/S21_math_lib/tree/master - Another S21_math implementation. All functions are separated
+ 
+ https://www.youtube.com/watch?v=sQt9ZP7u5m4 - math.h lecture from Sber students
+ 
+ 
+ 
+ 
+*/
+
+
+
+int main()
+{
+    printf("%Lf\n", s21_exp(5.0));
+    printf("%Lf\n", s21_fmod(99,4));
+    printf("%Lf\n", s21_ceil(59.234));
+    printf("%Lf\n", s21_ceil(-59.234));
+    printf("%Lf\n", s21_floor(49.234));
+    printf("%Lf\n", s21_floor(-49.234));
+    printf("%Lf\n", s21_sqrt(49));
     return 0;
 }
+
+// для инт ✅
+int s21_abs(int x) {
+    if (x < 0)  {
+        return x * (-1);
+    } else {
+        return x;
+    // Поставить защиту от дурака
+}
+
+
+
+// для double ✅
+long double s21_fabs(double x) {
+    if (x < 0) return x * (-1);
+    else return x;
+    // Поставить защиту от дураков
+}
+
+// Переделать на основе POW ✅
+long double s21_sqrt(double n) {
+    const double eps = 1e-9; // epsilon число
+    double x = 1;
+    while (s21_abs(x * x - n) > eps)
+        x = (x + n / x) / 2;
+    return x;
+}
+
+
+// Округляет число в большую сторону ✅
+long double s21_ceil(double x) {
+    if (x < 0){
+        double temp = s21_fabs(x);
+        while (temp > 1.0){
+        temp = temp - 1.0;
+        
+    }
+        return x  + temp;
+    }
+    double prev = x;
+    while (prev > 1.0){
+        prev = prev - 1.0;
+    }
+
+    return x + 1.0 - prev;
+}
+
+// Округляет в меньшую сторону ✅
+long double s21_floor(double x) {
+    if (x < 0){
+        double temp = s21_fabs(x);
+        while (temp > 1.0){
+        temp = temp - 1.0;
+        
+    }
+        return x  - 1 + temp;
+    } else {
+    double temp = x;
+    while (temp > 1.0){
+        temp = temp - 1.0;
+    }
+    
+    return x  - temp;
+    }
+}
+
+// Остаток от деления для чисел с плавающей точкой ✅
+long double s21_fmod(double x, double y) {
+    int temp = x/y;
+    int res = x - temp * y;
+    return res;
+}
+
+// Нахождение экспоненты
+long double s21_exp(double x) {
+    long double result = 1.0;
+    long double term = 1.0;
+    int i = 1;
+    const double eps = 1e-9;
+
+    while (1) {
+        term *= x / i;
+        result += term;
+        if (term < eps && term > -eps)
+            break;
+        i++;
+    }
+
+    return result;
+}
+
